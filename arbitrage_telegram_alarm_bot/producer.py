f82c450061576b2f7c3b87b0d133f5fc946116c4
JuhanKimSeoul <rlawngksss@gmail.com>
2024-12-23 02:04:32 +0900

    [Update] handle asyncio event loop more robustly in message processing

--- arbitrage_telegram_alarm_bot/producer.py
+++ arbitrage_telegram_alarm_bot/producer.py
@@ -31,11 +31,17 @@ def handle_message(message):
         data = json.loads(message['data'])
         # 결과 처리 로직 추가
         k = KimpManager()
-        loop = asyncio.get_event_loop()
-        if loop.is_closed():
+        try:
+            loop = asyncio.get_event_loop()
+            if loop.is_closed():
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+            loop.run_until_complete(k.send_telegram(data))
+        except RuntimeError as e:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-        loop.run_until_complete(k.send_telegram(data))
+            loop.run_until_complete(k.send_telegram(data))
+            
 
 def subscribe_to_redis():
     redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
